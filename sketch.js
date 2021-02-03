ec801872e0eb1d2d246f7727b52370caebe7cfc5
rasleenkaur1 <76608442+rasleenkaur1@users.noreply.github.com>
2021-02-03 16:44:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,42 @@
-var car , wall;
+var bullet , wall,thickness;
 var speed , weight;
 var d;
 function setup() {
   createCanvas(1600,400);
   //createSprite(400, 200, 50, 50);
-  speed = random(55,90);
-  weight = random(400,1500);
-  car = createSprite(50,200,50,50);
-  wall = createSprite(1200,200,60,height/2);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness= random(22,83);
+  bullet = createSprite(50,200,50,50);
+  wall = createSprite(1200,200,thickness,height/2);
   wall.shapeColor =color (80,80,80);
-  car.velocityX = speed;
-  d = 0.5*weight*speed*speed/22500;
+  bullet.velocityX = speed;
+  
 }
 
 function draw() {
   background("green");  
-  if(wall.x-car.x<(car.width+wall.width)/2){
-  car.velocityX = 0;
-  d = 0.5*weight*speed*speed/22500;
-  if(d>180){
-  car.shapeColor = color(255,0,0);
+  if(hasCollided(bullet,wall)){
+  bullet.velocityX = 0;
+  d = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if(d>10){
+  wall.shapeColor = color(255,0,0);
   }
-  if(d<180 && d>100){
-    car.shapeColor = color(230,230,0);
-    }
-    if(d<100){
-      car.shapeColor = color(0,255,0);
+  
+    if(d<10){
+      wall.shapeColor = color(0,255,0);
       }
   }
   drawSprites();
+}
+function hasCollided(bullet,wall){
+bulletRightEdge = bullet.x+bullet.width;
+wallLeftEdge = wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true;
+}
+return false;
+
+
+
 }
